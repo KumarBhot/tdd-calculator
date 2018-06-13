64766dcbc5bd6cb89d602d215a96c214d467d76a
Kumar Bhot <kumarbhot@yahoo.com>
2018-06-13 15:43:08 +0530

    pass - have results and buttons sections

--- app/Calculator.vue
+++ app/Calculator.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="calculator">
+        <div class="header">Header</div>
+        <div class="results">Results</div>
+        <div class="buttons">Buttons</div>
     </div>
 </template>
 
@@ -17,9 +20,30 @@
 
 <style scoped>
 .calculator {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  margin: 1rem;
-  max-width: 400px;
-  border: 1px solid darkslategrey;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    margin: 1rem;
+    max-width: 400px;
+    border: 1px solid darkslategrey;
+    border-radius: 0.25rem;
+}
+
+.header,
+.results,
+.buttons {
+    padding: 0.5rem;
+}
+
+.header {
+    background-color: lightslategrey;
+    color: white;
+    font-weight: bold;
+}
+
+.results {
+    color: darkolivegreen;
+}
+
+.buttons {
+    background-color: lightgrey;
 }
 </style>
