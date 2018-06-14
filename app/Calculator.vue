be577a7eb57f7497b63a33096d79b36dc25de8b4
Kumar Bhot <kumarbhot@yahoo.com>
2018-06-14 09:57:39 +0530

    pass - memory, extra and standard sections

--- app/Calculator.vue
+++ app/Calculator.vue
@@ -1,8 +1,49 @@
 <template>
     <div class="calculator">
-        <div class="header">Header</div>
-        <div class="results">Results</div>
-        <div class="buttons">Buttons</div>
+        <div class="header">Calculator</div>
+        <div class="results">0</div>
+        <div class="buttons">
+            <div class="memory-ops">
+                <button value="mc">MC</button>
+                <button value="mr">MR</button>
+                <button value="m+">M+</button>
+                <button value="m-">M-</button>
+                <button value="ms">MS</button>
+                <button value="m">M<sup>▼</sup></button>
+            </div>
+            <div class="extra-ops">
+                <button value="mod">%</button>
+                <button value="sqrt">√</button>
+                <button value="sqr">x<sup>2</sup></button>
+                <button value="inverse">1/x</button>
+            </div>
+            <div class="standard-ops">
+                <button value="ce">CE</button>
+                <button value="c">C</button>
+                <button value="erase">←</button>
+                <button value="divide">÷</button>
+
+                <button class="operand" value="7">7</button>
+                <button class="operand" value="8">8</button>
+                <button class="operand" value="9">9</button>
+                <button class="operator" value="multiply">x</button>
+
+                <button class="operand" value="4">4</button>
+                <button class="operand" value="5">5</button>
+                <button class="operand" value="6">6</button>
+                <button class="operator" value="subtract">-</button>
+
+                <button class="operand" value="1">1</button>
+                <button class="operand" value="2">2</button>
+                <button class="operand" value="3">3</button>
+                <button class="operator" value="add">+</button>
+
+                <button class="operator" value="sign">±</button>
+                <button class="operand" value="0">0</button>
+                <button class="operand" value=".">.</button>
+                <button class="operator" value="equals">=</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,6 +68,10 @@
     border-radius: 0.25rem;
 }
 
+.calculator sup {
+    font-size: 50%;
+}
+
 .header,
 .results,
 .buttons {
@@ -40,10 +85,36 @@
 }
 
 .results {
+    text-align: right;
     color: darkolivegreen;
+    font-size: 200%;
 }
 
 .buttons {
     background-color: lightgrey;
+}
+
+.buttons button {
+    width: 24%;
+}
+
+.memory-ops button {
+    width: 16%;
+}
+
+.memory-ops {
+    font-size: .75%;
+}
+
+.extra-ops {
+    margin: 0.25rem 0;
+    padding: 0.25rem 0;
+    border: 1px solid white;
+    border-left-width: 0;
+    border-right-width: 0;
+}
+
+.standard-ops button {
+    font-weight: bold;
 }
 </style>
